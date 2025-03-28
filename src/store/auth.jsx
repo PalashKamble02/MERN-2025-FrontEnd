4340de124dc80838ec15f897c10472460804b99a
palashkamble <kamblepalash356@gmail.com>
2025-03-28 13:45:23 +0530

    fronend 1.8

--- src/store/auth.jsx
+++ src/store/auth.jsx
@@ -1,13 +1,23 @@
-import {  createContext, useContext } from "react";
+import {  createContext, useContext, useState } from "react";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({children}) =>{
+    const [token , setToken]= useState(localStorage.getItem("token"));
     const storeTokenInLS = (serverToken) =>{
         return localStorage.setItem("token", serverToken)
     };
 
-    return <AuthContext.Provider value={{storeTokenInLS}}>
+
+    let isLoggedIn = !!token;
+
+    //logout logic
+    const LogoutUser = ()=>{
+        setToken("");
+        return localStorage.removeItem("token");
+    };
+
+    return <AuthContext.Provider value={{isLoggedIn, storeTokenInLS, LogoutUser}}>
      {children}
     </AuthContext.Provider>
 }
